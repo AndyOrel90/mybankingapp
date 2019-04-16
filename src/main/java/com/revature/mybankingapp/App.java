0da906daf57b17c32d0faf05c338824328072450
MCP\Andres <AndyOrel90@gmail.com>
2019-04-16 15:08:41 -0500

    First Commit

---
+++ src/main/java/com/revature/mybankingapp/App.java
@@ -0,0 +1,14 @@
+package com.revature.mybankingapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//new
+        System.out.println( "Hello World!" );
+    }
+}
